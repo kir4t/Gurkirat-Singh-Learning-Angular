6072ee424833a65c25659a05aba9bfd0cf0b213a
w0830149 <g.skiratsingh922@gmail.com>
2024-09-12 19:15:17 -0400

    Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Gurkirat-Singh-Learning-Angular';
+  name :string = "Gurkirat";
+  collegeID : number = 830149;
+
 }
