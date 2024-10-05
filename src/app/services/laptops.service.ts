c9d79efefe57ea0758ac711ea27222dcbdee8ea7
Gurkirat Singh <W0830149@myscc.ca>
2024-10-04 22:59:03 -0400

    done till step 9

--- src/app/services/laptops.service.ts
+++ src/app/services/laptops.service.ts
@@ -11,8 +11,28 @@ export class LaptopsService {
 
   constructor() { }
   getLaptops():Observable<Laptops[]>{
-    return of (laptopsArray);
+    return of (this.laptops);
 
   }
+  addLaptop(newLaptop: Laptops): Observable<Laptops[]>{
+    laptopsArray.push(newLaptop);
+    return of(this.laptops);
 
+  }
+  updateLaptop(updatedLaptops:Laptops): Observable<Laptops[]> {
+    const index = laptopsArray.findIndex(laptop => laptop.serialNumber === updatedLaptops.serialNumber);
+    if (index !== -1) {
+      laptopsArray[index] = updatedLaptops;
+    }
+    return of(this.laptops);
+  }
+
+  deleteLaptop(serialNumber:string): Observable<Laptops[]>{
+    this.laptops = laptopsArray.filter(laptop=>laptop.serialNumber !== serialNumber);
+    return of (this.laptops);
+  }
+  getLaptopBySerialNumber(serialNumber:string): Observable<Laptops | undefined>{
+    const laptop = laptopsArray.find(laptop=>laptop.serialNumber === serialNumber);
+    return of(laptop);
+  }
 }
