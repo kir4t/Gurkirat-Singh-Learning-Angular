a0bb8b9e24c42bc236eae34812bdc55dc5dd9230
Gurkirat Singh <W0830149@myscc.ca>
2024-11-07 16:21:16 -0500

    added data in memory-data-service

---
+++ src/app/services/in-memory-data-service.ts
@@ -0,0 +1,23 @@
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Laptops} from "../Shared/Models/Laptops";
+import {Observable} from "rxjs";
+
+export class InMemoryDataService implements InMemoryDbService{
+
+  createDb(): {laptopsArray: Laptops[]}{
+    const laptopsArray: Laptops[] = [
+      { serialNumber: 1111, brand: "Apple", model: "mac pro", storage: 256, isAvailable: true, image:"/assets/apple1.jpg" },
+      { serialNumber: 2222, brand: "Hp", model: "Pavilion", storage: 512, isAvailable: true, image:"/assets/hp.jpg"},
+      { serialNumber: 3333, brand: "Think Pad", model: "X1 carbon", storage: 256, isAvailable: true, image:"/assets/thinkpad.jpeg" },
+      { serialNumber: 4444, brand: "Lenovo", model: "G2 max", storage: 256, isAvailable: false , image:"/assets/lenovo.jpg"},
+      { serialNumber: 5555, brand: "Asus", model: "book 15", storage: 512, isAvailable: false , image:"/assets/asus.jpg"},
+      { serialNumber: 6666, brand: "Apple", model: "mac pro max", storage: 512, isAvailable: false, image:"/assets/apple2.jpg" }
+    ];
+    return {laptopsArray};
+  }
+
+
+
+
+
+}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app/app.component';
 import {LaptopsListComponent} from "./app/laptops-list/laptops-list.component";
 import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {provideHttpClient} from "@angular/common/http";
+import {importProvidersFrom} from "@angular/core";
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./app/services/in-memory-data-service";
 
 
 const routes: Routes =[
@@ -14,5 +18,9 @@ const routes: Routes =[
   {path: '**', component:PageNotFoundComponent}
 ]
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-}).then(()=> console.log('Bootstrap successful'));
+  providers: [
+    provideHttpClient(), // Ensure that HTTP interceptors are properly configured
+    provideRouter(routes),
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)) // Import providers dynamically
+  ],
+}).catch((err) => console.error(err));
